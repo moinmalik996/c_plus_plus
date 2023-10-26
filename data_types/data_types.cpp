5556be0b1df6e52a3ae3e99ca66ed4e5bd0b777f
moin abbas <moin.malik996@gmail.com>
2023-10-27 02:42:22 +0500

    sum program

---
+++ data_types/data_types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+    int x{0};
+    int y{0};
+
+
+    cout<<"Enter value for x: ";
+    cin>>x;
+
+    cout<<"Enter value for y: ";
+    cin>>y;
+
+    cout<<"\nThe Sum is = "<< x + y<<"\n\n";
+
+
+    return 0;
+
+}
